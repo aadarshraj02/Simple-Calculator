96c6c83e9becee0232935817de5cc7d307302c72
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-23 15:55:47 +0530

    added for each for every button value and pushed that value to screen

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-(function(){
-    let screen = document.querySelector(".screen")
-    let button = document.querySelectorAll(".btn")
-    let clear = document.querySelector(".btn-clear")
-    let equal = document.querySelector(".btn-equal")
+(function () {
+  let screen = document.querySelector(".screen");
+  let buttons = document.querySelectorAll(".btn");
+  let clear = document.querySelector(".btn-clear");
+  let equal = document.querySelector(".btn-equal");
+
+  buttons.forEach(function (button) {
+    button.addEventListener("click", (e) => {
+      let value = e.target.dataset.num;
+      screen.value += value;
+    });
+  });
 })();
