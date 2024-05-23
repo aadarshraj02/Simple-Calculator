5555ca9946330c8501cc148a92215afe02db370b
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-23 15:51:12 +0530

    selected buttons and screen of html and stored in variables

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 (function(){
-
+    let screen = document.querySelector(".screen")
+    let button = document.querySelectorAll(".btn")
+    let clear = document.querySelector(".btn-clear")
+    let equal = document.querySelector(".btn-equal")
 })();
